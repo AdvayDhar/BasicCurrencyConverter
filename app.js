7d02afd4b0fb507c364ce6a3f945aa7c6098fc9d
AdvayDhar <advay.codegod@gmail.com>
2024-09-09 17:24:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,75 @@
+
+
+BASE_URL="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+
+
+const dropdowns=document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr=document.querySelector(".from select");
+const toCurr=document.querySelector(".to select");
+const message= document.querySelector(".info")
+
+window.addEventListener("load", ()=>{
+    btn.click();
+}
+);
+
+for (let select of dropdowns){
+    for (currCode in countryList) {
+        console.log(currCode);
+        let newOption = document.createElement("option");
+        newOption.innerText=currCode;
+        newOption.value=currCode;
+        select.append(newOption);
+        if (select.name==="from"&& currCode==="USD"){
+            newOption.selected="selected";
+        }else if (select.name==="to" && currCode==="INR"){
+            newOption.selected="selected";
+        }
+
+    }
+
+    select.addEventListener("change", (evt)=>{
+        updateFlag(evt.target);
+    });
+    
+}
+
+const updateFlag = (element)=>{
+    let currCode = element.value;
+    console.log(currCode);
+    let countryCode = countryList [currCode];
+    let newSrc=`https://flagsapi.com/${countryCode}/flat/64.png`;
+
+    let img= element.parentElement.querySelector("img");
+    img.src= newSrc;
+}
+
+btn.addEventListener("click", async (evt)=>{
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtVal=amount.value;
+    console.log(amtVal);
+    if (amtVal==="" || amtVal<1){
+        amtVal=1;
+        amount.value="1";
+    }
+//     
+ console.log(fromCurr.value, toCurr.value);
+
+const URL=`${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+let response= await fetch(URL);
+console.log(response);
+let data= await response.json();
+console.log(data);
+console.log(data);
+let rate= await data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+console.log(rate);
+let finalAmt=amtVal* rate;
+
+message.innerText=`${amtVal} ${fromCurr.value} = ${finalAmt} ${toCurr.value}`
+
+
+
+});
+
